639d3a28015e4b37057aacfc93b08c350aa116ca
Divyank Kulshrestha <52440757+devDivyank@users.noreply.github.com>
2020-12-20 23:02:51 +0530

    Create SubCipher.py

---
+++ SubstitutionCipher/SubCipher.py
@@ -0,0 +1,127 @@
+import string
+
+def load_words(file_name):
+    '''
+    file_name (string): the name of the file containing
+    the list of words to load
+
+    Returns: a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    '''
+
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(file_name, 'r')
+    # wordlist: list of strings
+    wordlist = []
+    for line in inFile:
+        wordlist.extend([word.lower() for word in line.split(' ')])
+    print("  ", len(wordlist), "words loaded.")
+    return wordlist
+
+def is_word(word_list, word):
+    '''
+    Determines if word is a valid word, ignoring
+    capitalization and punctuation
+
+    word_list (list): list of words in the dictionary.
+    word (string): a possible word.
+
+    Returns: True if word is in word_list, False otherwise
+
+    Example:
+    >>> is_word(word_list, 'bat') returns
+    True
+    >>> is_word(word_list, 'asdf') returns
+    False
+    '''
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in word_list
+
+WORDLIST_FILENAME = 'words.txt'
+
+VOWELS_LOWER = 'aeiou'
+VOWELS_UPPER = 'AEIOU'
+CONSONANTS_LOWER = 'bcdfghjklmnpqrstvwxyz'
+CONSONANTS_UPPER = 'BCDFGHJKLMNPQRSTVWXYZ'
+
+
+class SubMessage(object):
+    def __init__(self, text):
+        '''
+        Initializes a SubMessage object
+
+        text (string): the message's text
+
+        A SubMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class.
+        This helps you avoid accidentally mutating class attributes.
+
+        Returns: a COPY of self.valid_words
+        '''
+        mylist = []
+        for i in self.valid_words:
+            mylist.append(i)
+        return mylist
+
+    def build_transpose_dict(self, vowels_permutation):
+        '''
+        vowels_permutation (string): a string containing a permutation of vowels (a, e, i, o, u)
+
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to an
+        uppercase and lowercase letter, respectively. Vowels are shuffled
+        according to vowels_permutation. The first letter in vowels_permutation
+        corresponds to a, the second to e, and so on in the order a, e, i, o, u.
+        The consonants remain the same. The dictionary should have 52
+        keys of all the uppercase letters and all the lowercase letters.
+
+        Example: When input "eaiuo":
+        Mapping is a->e, e->a, i->i, o->u, u->o
+        and "Hello World!" maps to "Hallu Wurld!"
+
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
+        '''
+        mydict = {}
+        vowels_all = VOWELS_UPPER + VOWELS_LOWER
+        vowels_permutation_all = vowels_permutation.upper() + vowels_permutation.lower()
+        for i in range(len(vowels_all)):
+            mydict[vowels_all[i]] = vowels_permutation_all[i]
+        for i in CONSONANTS_UPPER+CONSONANTS_LOWER:
+            mydict[i] = i
+        return mydict
+
+    def apply_transpose(self, transpose_dict):
+        '''
+        transpose_dict (dict): a transpose dictionary
+
+        Returns: an encrypted version of the message text, based
+        on the dictionary
+        '''
+        mystr = ''
+        for i in self.message_text:
+            if i not in string.ascii_uppercase and i not in string.ascii_lowercase:
+                mystr += i
+            else:
+                mystr += transpose_dict[i]
+        return mystr
